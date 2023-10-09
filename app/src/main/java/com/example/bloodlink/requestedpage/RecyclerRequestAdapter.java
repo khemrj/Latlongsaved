94188433411788417f1170d4108922f556ccf8d4
SUMAN-SHAH-CODE <sumanshahst@gmail.com>
2023-10-10 00:11:08 +0545

    package error is been solved

--- app/src/main/java/com/example/bloodlink/requestedpage/RecyclerRequestAdapter.java
+++ app/src/main/java/com/example/bloodlink/requestedpage/RecyclerRequestAdapter.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.login.R;
+import com.example.bloodlink.R;
 
 import java.util.ArrayList;
 
