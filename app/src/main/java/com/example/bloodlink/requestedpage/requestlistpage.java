27cb5e642a3c3a6b4e30345a5d1510e284762bfb
SUMAN-SHAH-CODE <sumanshahst@gmail.com>
2023-10-10 00:10:35 +0545

    package error is been solved

--- app/src/main/java/com/example/bloodlink/requestedpage/requestlistpage.java
+++ app/src/main/java/com/example/bloodlink/requestedpage/requestlistpage.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.login.R;
+import com.example.bloodlink.R;
 
 import java.util.ArrayList;
 
