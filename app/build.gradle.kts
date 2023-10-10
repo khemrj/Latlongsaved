ae30f60d2f8cb275005aff30b99df515a7aea1bd
SUMAN-SHAH-CODE <sumanshahst@gmail.com>
2023-10-10 13:46:26 +0545

    implementing all

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.bloodlink"
-    compileSdk = 33
+    compileSdk = 34
     buildFeatures{
         viewBinding=true
     }
@@ -35,8 +35,10 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
