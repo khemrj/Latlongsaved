6831e0e41abba7fdbe14f864f33e52f579be8664
SUMAN-SHAH-CODE <sumanshahst@gmail.com>
2023-10-10 23:00:14 +0545

    dashbord code size correction

--- app/src/main/java/com/example/bloodlink/dashboard/dashboard.java
+++ app/src/main/java/com/example/bloodlink/dashboard/dashboard.java
@@ -3,6 +3,7 @@ package com.example.bloodlink.dashboard;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +12,7 @@ import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.bloodlink.R;
 import com.example.bloodlink.databinding.ActivityDashboardBinding;
+import com.example.bloodlink.donorpage.donorPage;
 import com.example.bloodlink.myprofile.myprofile;
 import com.example.bloodlink.searchdonor.searchdonor;
 
@@ -18,18 +20,28 @@ import java.util.ArrayList;
 
 public class dashboard extends AppCompatActivity {
 ActivityDashboardBinding binding;
-
+    ImageButton notify;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityDashboardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        notify = findViewById(R.id.notification);
+        notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(dashboard.this, donorPage.class);
+                startActivity(i);
+            }
+        });
+
         ImageSlider imageSlider=binding.imageSlider;
         ArrayList<SlideModel>slideModels=new ArrayList<>();
         slideModels.add(new SlideModel(R.drawable.bl1, ScaleTypes.FIT));
         slideModels.add(new SlideModel(R.drawable.b2, ScaleTypes.FIT));
         imageSlider.setImageList(slideModels,ScaleTypes.FIT);
+
 
         binding.requested.setOnClickListener(new View.OnClickListener() {
             @Override
