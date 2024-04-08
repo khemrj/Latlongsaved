da188ced5404e76e2bd495871eb52a23444b6b97
SUMAN-SHAH-CODE <sumanshahst@gmail.com>
2024-04-08 21:37:50 +0545

    done map fragment location image button

--- app/src/main/java/com/example/bloodlink/donorpage/location.java
+++ app/src/main/java/com/example/bloodlink/donorpage/location.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,10 +21,11 @@ public class location extends AppCompatActivity {
         setContentView(R.layout.donor_row);
 
 
-        imageButton = findViewById(R.id.locationPin);
+        imageButton =(ImageButton) findViewById(R.id.locationPin);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(location.this, "button pin location clicked ", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(location.this, MapsActivity.class);
                 startActivity(intent);
             }
